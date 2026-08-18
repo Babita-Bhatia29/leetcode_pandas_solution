80d9222aaa68f81cd946d19b7e87533a0e04adad
Babita Bhatia <113882363+Babita-Bhatia29@users.noreply.github.com>
2026-08-18 19:55:52 +0530

    Time: 274 ms (95.91%), Space: 67.5 MB (44.32%) - LeetHub

---
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+  
+    valid_mail_df=users[users["mail"].str.match(r'^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\?com)?\.com$')]
+    return valid_mail_df
+    
